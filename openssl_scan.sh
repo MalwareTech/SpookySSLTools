1a93438338898e6b37042a7b5906d3b91e5c3ae4
CanadaHonk <19228318+CanadaHonk@users.noreply.github.com>
2022-11-01 09:37:08 +0000

    Bash: Fix incorrect quotes and syntax

--- openssl_scan.sh
+++ openssl_scan.sh
@@ -9,17 +9,17 @@ only_vulnerable=0
 echo This is an example script not meant for production use. To confirm that you understand and accept all responsibility, type: confirm
 read confirm
 
-if [[ “$confirm” == “confirm” ]]; then 
+if [[ "$confirm" == "confirm" ]]; then 
     if [[ $only_vulnerable -eq 1 ]]; then
         regex="^OpenSSL\s*3.0.[0-6]"
     else
         regex="^OpenSSL\s*[0-9].[0-9].[0-9]"
     fi
 
-    # install binutils (contains ‘strings’ command)
+    # install binutils (contains 'strings' command)
     apt-get install binutils
 
-    for file_name in $(find $search_directory -type f -name "libcrypto*.so*" -o -name “libssl*.so*” -o -name “libssl*.a* -o -name “libcrypto*.a*); do
+    for file_name in $(find $search_directory -type f -name "libcrypto*.so*" -o -name "libssl*.so*" -o -name "libssl*.a*" -o -name "libcrypto*.a*"); do
         openssl_version=$( strings $file_name | grep $regex)
         if [[ $openssl_version ]]; then
             echo  $openssl_version - $file_name
